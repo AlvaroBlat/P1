cde317fae10c9400fe55d6d2d2042577cf39ca7d
Enric_Gil_Gallen <gui.encoding>
2022-10-25 12:49:53 +0200

    Haber si ahora funcina las ventanas

---
+++ ProyectoISII/src/vista/pruebaEnfermera.java
@@ -0,0 +1,8 @@
+package vista;
+
+import javax.swing.*;
+
+public class pruebaEnfermera {
+    private JButton administrarMedicamentosPorFechaButton;
+    private JButton consultarBotiquinButton;
+}
